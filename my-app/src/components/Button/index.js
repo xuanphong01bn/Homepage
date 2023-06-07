a5dccad69aa5fa1c4f4fb8f31db34d069fa63467
xuanphong01bn <donhatphong94@gmail.com>
2023-06-08 00:53:44 +0700

    add-component

---
+++ my-app/src/components/Button/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: "#F3C003",
+          padding: "18px 24px",
+          borderRadius: "50%",
+        }}
+      >
+        Xem thêm
+      </div>
+    </div>
+  );
+};
+
+export default Button;
